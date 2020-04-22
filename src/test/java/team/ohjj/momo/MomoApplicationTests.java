6af8dda3fadbb7d035d1bb80e7ca496fd337a811
Jimen <hurjeesic@naver.com>
2020-04-22 18:08:40 +0900

    프로젝트 생성

---
+++ src/test/java/team/ohjj/momo/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package team.ohjj.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
