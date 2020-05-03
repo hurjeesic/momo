c0caf10bde1f83c0244d0ad087d8a7fe1bca7386
Jimen <hurjeesic@naver.com>
2020-05-03 20:09:48 +0900

    참조 데이터형에 대하여  Wrapper 클래스로 변경 및 Login api 구현

--- src/main/java/team/ohjj/momo/entity/UserRepository.java
+++ src/main/java/team/ohjj/momo/entity/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.ohjj.momo.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmailAndPasswordAndType(String email, String password, Byte type);
 }
--- src/main/java/team/ohjj/momo/restapi/UserRestController.java
+++ src/main/java/team/ohjj/momo/restapi/UserRestController.java
@@ -5,13 +5,33 @@ import org.springframework.web.bind.annotation.*;
 import team.ohjj.momo.domain.User;
 import team.ohjj.momo.entity.UserRepository;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/user")
 public class UserRestController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/user/{id}")
+    @PostMapping("/login")
+    public Integer login(@RequestParam Map<String, Object> param) {
+        Integer id = 0;
+
+        try {
+            String email = (String) param.get("email");
+            String password = (String) param.get("password");
+            Byte type = Byte.parseByte((String) param.get("type"));
+
+            id = userRepository.findByEmailAndPasswordAndType(email, password, type).getNo();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return id;
+    }
+
+    @GetMapping("/{id}")
     public User getUserInfo(@PathVariable Integer id) {
         User user = null;
 
