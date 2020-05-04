1d4d93bd64ae7ed7daaddffaaadf6fddddd2190a
Jimen <hurjeesic@naver.com>
2020-05-05 08:50:54 +0900

    이메일 전송을 담당하는 MailHandler 클래스 구현

---
+++ src/main/java/team/ohjj/momo/mail/TempKey.java
@@ -0,0 +1,46 @@
+package team.ohjj.momo.mail;
+
+import java.util.Random;
+
+public class TempKey {
+    private boolean lowerCheck;
+    private int size;
+
+    private static TempKey instance = new TempKey();
+
+    private TempKey() {
+
+    }
+
+    public static TempKey getInstance() {
+        return instance;
+    }
+
+    public String getKey(int size, boolean lowerCheck) {
+        this.size = size;
+        this.lowerCheck = lowerCheck;
+
+        return init();
+    }
+
+    private String init() {
+        Random rand = new Random();
+        StringBuffer sb = new StringBuffer();
+        int num = 0;
+        do {
+            num = rand.nextInt(75) + 48;
+            if ((num >= 48 && num <= 57) || (num >= 65 && num <= 90) || (num >= 97 && num <= 122)) {
+                sb.append((char)num);
+            }
+            else {
+                continue;
+            }
+        } while (sb.length() < size);
+
+        if (lowerCheck) {
+            return sb.toString().toLowerCase();
+        }
+
+        return sb.toString();
+    }
+}
