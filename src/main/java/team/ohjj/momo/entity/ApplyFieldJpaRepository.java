40fe674c639f776c001de93ce09a1eae8f1af699
Jimen <hurjeesic@naver.com>
2020-05-27 02:47:52 +0900

    프로젝트 생성에서의 지원 직군 관리를 위한 ApplyField 객체 정의 및 기본 Repository 생성

---
+++ src/main/java/team/ohjj/momo/entity/ApplyFieldJpaRepository.java
@@ -0,0 +1,8 @@
+package team.ohjj.momo.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.ohjj.momo.domain.ApplyField;
+
+public interface ApplyFieldJpaRepository extends JpaRepository<ApplyField, Integer> {
+
+}
