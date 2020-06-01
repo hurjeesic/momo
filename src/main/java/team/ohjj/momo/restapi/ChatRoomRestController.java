5916708110791a98a827355ecd2a351f771053c2
Jimen <hurjeesic@naver.com>
2020-06-01 22:18:13 +0900

    대화방 만들기 session에 따른 보안 강화

--- src/main/java/team/ohjj/momo/restapi/ChatRoomRestController.java
+++ src/main/java/team/ohjj/momo/restapi/ChatRoomRestController.java
@@ -3,10 +3,13 @@ package team.ohjj.momo.restapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import team.ohjj.momo.domain.ChatRoom;
+import team.ohjj.momo.domain.Member;
 import team.ohjj.momo.entity.ChatRoomJpaRepository;
 import team.ohjj.momo.entity.ChatRoomRepository;
+import team.ohjj.momo.entity.MemberJpaRepository;
 import team.ohjj.momo.entity.ProjectJpaRepository;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -21,6 +24,9 @@ public class ChatRoomRestController {
     @Autowired
     private ProjectJpaRepository projectJpaRepository;
 
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+
     @GetMapping("/{id}")
     public ChatRoom getChatRoom(@PathVariable String id) {
         return chatRoomJpaRepository.findById(id).get();
@@ -32,7 +38,19 @@ public class ChatRoomRestController {
     }
 
     @PutMapping("/insert")
-    public ChatRoom createChatRoom(@RequestParam String chatRoomName, @RequestParam Integer projectId) {
+    public ChatRoom createChatRoom(HttpSession session, @RequestParam String chatRoomName, @RequestParam Integer projectId) throws Exception {
+        boolean bAuth = false;
+        for (Member member: memberJpaRepository.findAllByProject(projectJpaRepository.findById(projectId).get())) {
+            if (member.getUser().equals(session.getAttribute("user"))) {
+                bAuth = true;
+                break;
+            }
+        }
+
+        if (bAuth) {
+            throw new Exception();
+        }
+
         return chatRoomRepository.createChatRoom(chatRoomName, projectId);
     }
 
