593482b752a13065d472ae3611e43d7a3e44601a
Jimen <hurjeesic@naver.com>
2020-06-12 23:45:04 +0900

    지원자 승인 기능 구현

--- src/main/java/team/ohjj/momo/restapi/ApplicantRestController.java
+++ src/main/java/team/ohjj/momo/restapi/ApplicantRestController.java
@@ -3,12 +3,10 @@ package team.ohjj.momo.restapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import team.ohjj.momo.domain.Applicant;
+import team.ohjj.momo.domain.Member;
 import team.ohjj.momo.domain.Project;
 import team.ohjj.momo.domain.User;
-import team.ohjj.momo.entity.ApplicantJpaRepository;
-import team.ohjj.momo.entity.ApplyFieldJpaRepository;
-import team.ohjj.momo.entity.ProjectJpaRepository;
-import team.ohjj.momo.entity.UserJpaRepository;
+import team.ohjj.momo.entity.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -29,6 +27,9 @@ public class ApplicantRestController {
 	ApplyFieldJpaRepository applyFieldJpaRepository;
 
 	@Autowired
+	MemberJpaRepository memberJpaRepository;
+
+	@Autowired
 	ApplicantJpaRepository applicantJpaRepository;
 
 	@GetMapping("/{no}")
@@ -36,17 +37,39 @@ public class ApplicantRestController {
 		return applicantJpaRepository.findAllByProject(projectJpaRepository.findById(no).get());
 	}
 
+	@PostMapping("/complete")
+	public List<Member> completeApply(HttpSession session, @RequestParam(value = "applicantNums[]") List<Integer> applicantNums) {
+		User user = (User)session.getAttribute("user");
+		List<Member> confirmMembers = new ArrayList<>();
+		for (Integer no : applicantNums) {
+			Applicant applicant = applicantJpaRepository.findById(no).get();
+
+			if (user.equals(applicant.getProject().getOrganizer())) {
+				applicantJpaRepository.deleteById(no);
+				if (!applicantJpaRepository.findById(no).isPresent()) {
+					try {
+						Member member = new Member();
+						member.setProject(applicant.getProject());
+						member.setUser(applicant.getUser());
+						member.setField(applicant.getField());
+						confirmMembers.add(memberJpaRepository.save(member));
+					}
+					catch (Exception e) {
+						applicantJpaRepository.save(applicant);
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+		return confirmMembers;
+	}
+
 	@PostMapping("/insert")
 	public Boolean createApplicant(HttpSession session, @ModelAttribute Applicant applicant) {
 		applicant.setUser(((User)session.getAttribute("user")));
 
 		applicantJpaRepository.save(applicant);
-
-		return true;
-	}
-
-	public Boolean deleteApplicant(HttpSession session, int applicantNo) {
-		applicantJpaRepository.deleteById(applicantNo);
 
 		return true;
 	}
