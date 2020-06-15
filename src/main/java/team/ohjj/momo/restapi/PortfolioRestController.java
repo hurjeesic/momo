d42932fea84ac4d9d87d5a2e48ddff5127dbaa8f
Jimen <hurjeesic@naver.com>
2020-06-16 01:00:26 +0900

    프로젝트 완료페이지 기능 구현

--- src/main/java/team/ohjj/momo/restapi/PortfolioRestController.java
+++ src/main/java/team/ohjj/momo/restapi/PortfolioRestController.java
@@ -1,13 +1,20 @@
 package team.ohjj.momo.restapi;
 
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import team.ohjj.momo.domain.Portfolio;
 import team.ohjj.momo.domain.User;
 import team.ohjj.momo.entity.PortfolioJpaRepository;
 import team.ohjj.momo.entity.ProjectJpaRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -25,12 +32,58 @@ public class PortfolioRestController {
 	}
 
 	@PostMapping("/insert")
-	public Boolean createPortfolio(HttpSession session, @RequestParam Portfolio portfolio) {
+	public Boolean createPortfolio(HttpServletRequest request, HttpSession session, @RequestParam MultipartFile file, @ModelAttribute Portfolio portfolio) {
 		User user = (User)session.getAttribute("user");
-		
-		portfolio.setUser(user);
-		portfolioJpaRepository.save(portfolio);
+		String path;
+		File projectFile;
+		FileOutputStream fileOutputStream = null;
+		BufferedOutputStream bufferedOutputStream = null;
 
-		return true;
+		try {
+			if (file != null) {
+				if (FilenameUtils.getExtension(file.getOriginalFilename()).equals("zip")) {
+					portfolio.setProjectFile(portfolio.getProject().getNo() + "_" + file.getOriginalFilename());
+					path = request.getServletContext().getRealPath("/") + "/WEB-INF/static/project/" + portfolio.getProjectFile();
+					projectFile = new File(path);
+					fileOutputStream = new FileOutputStream(projectFile);
+					bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+					bufferedOutputStream.write(file.getBytes());
+				}
+				else {
+					throw new RuntimeException("Extension name Exception");
+				}
+			}
+
+			portfolio.setUser(user);
+
+			return portfolioJpaRepository.save(portfolio) != null;
+		}
+		catch (IOException ie) {
+			ie.printStackTrace();
+		}
+		catch (RuntimeException re) {
+			re.printStackTrace();
+		}
+		finally {
+			try {
+				if (bufferedOutputStream != null) {
+					bufferedOutputStream.close();
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if (fileOutputStream != null) {
+					fileOutputStream.close();
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return false;
 	}
 }
