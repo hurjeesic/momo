92cf23d3bda364794ec716d9d88bcd332c7be1c1
Jimen <hurjeesic@naver.com>
2020-06-15 16:34:37 +0900

    포트폴리오 확인과 작성에 대한 rest api 작성

--- src/main/java/team/ohjj/momo/entity/PortfolioJpaRepository.java
+++ src/main/java/team/ohjj/momo/entity/PortfolioJpaRepository.java
@@ -5,8 +5,10 @@ import team.ohjj.momo.domain.Portfolio;
 import team.ohjj.momo.domain.Project;
 import team.ohjj.momo.domain.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PortfolioJpaRepository extends JpaRepository<Portfolio, Integer> {
 	Optional<Portfolio> findByProjectAndUser(Project project, User user);
+	List<Portfolio> findAllByUser(User user);
 }
--- src/main/java/team/ohjj/momo/restapi/PortfolioRestController.java
+++ src/main/java/team/ohjj/momo/restapi/PortfolioRestController.java
@@ -1,4 +1,36 @@
 package team.ohjj.momo.restapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import team.ohjj.momo.domain.Portfolio;
+import team.ohjj.momo.domain.User;
+import team.ohjj.momo.entity.PortfolioJpaRepository;
+import team.ohjj.momo.entity.ProjectJpaRepository;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/portfolio")
 public class PortfolioRestController {
+	@Autowired
+	ProjectJpaRepository projectJpaRepository;
+
+	@Autowired
+	PortfolioJpaRepository portfolioJpaRepository;
+
+	@GetMapping("/")
+	public List<Portfolio> getPortfolioList(HttpSession session) {
+		return portfolioJpaRepository.findAllByUser((User)session.getAttribute("user"));
+	}
+
+	@PostMapping("/insert")
+	public Boolean createPortfolio(HttpSession session, @RequestParam Portfolio portfolio) {
+		User user = (User)session.getAttribute("user");
+		
+		portfolio.setUser(user);
+		portfolioJpaRepository.save(portfolio);
+
+		return true;
+	}
 }
